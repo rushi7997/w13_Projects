1d2a3cc691996df301ed9ec3501e31fde5824f4b
Rushi Rami <ramirushi7997@gmail.com>
2020-11-04 20:32:12 -0500

    login done

--- web_server_1/server.js
+++ web_server_1/server.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let app = express();
 
+app.use(express.static('public_html'));
+app.use(express.urlencoded());
+app.use(express.json());
+
+
 app.get('/', async(req, res) => {
     try{
         res.send("<h1>Hello World</h1>");
@@ -8,6 +13,24 @@ app.get('/', async(req, res) => {
         res.status(500).send("<h1> Error! </h1>")
     }
 });
+
+app.post('/login', (request, response) => {
+    try{
+        // console.log(request.body);
+        const username = request.body.username;
+        const password = request.body.password;
+        const myUsername = 'rushi';
+        const myPassword = 'password';
+
+        if(username === myUsername && password === myPassword){
+            response.status(200).send("<h1>You are logged in!</h1>")
+        }else{
+            response.status(200).send('<h1>You entered Wrong Credentials</h1>')
+        }
+    }catch(e) {
+        response.status(500).send("Error" + e);
+    }
+})
 
 app.listen(8000, () => {
     console.log('Server listening to port 8000, go to https://localhost:8000');
