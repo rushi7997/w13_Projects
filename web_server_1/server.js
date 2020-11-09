bf69bc96b316b02d2316a8d05ce919d864ac0dec
Rushi Rami <ramirushi7997@gmail.com>
2020-11-09 14:01:32 -0500

    send file with chair get route

--- web_server_1/server.js
+++ web_server_1/server.js
@@ -1,35 +1,42 @@
+const { request, response } = require('express');
 let express = require('express');
 let app = express();
+const path = require('path');
 
 app.use(express.static('public_html'));
 app.use(express.urlencoded());
 app.use(express.json());
 
 
-app.get('/', async(req, res) => {
-    try{
+app.get('/', async (req, res) => {
+    try {
         res.send("<h1>Hello World</h1>");
-    }catch(e){
+    } catch (e) {
         res.status(500).send("<h1> Error! </h1>")
     }
 });
 
-app.post('/login', async(request, response) => {
-    try{
+app.post('/login', async (request, response) => {
+    try {
         // console.log(request.body);
         const username = request.body.username;
         const password = request.body.password;
         const myUsername = 'rushi';
         const myPassword = 'password';
 
-        if(username === myUsername && password === myPassword){
+        if (username === myUsername && password === myPassword) {
             response.status(200).send("<h1>You are logged in!</h1>")
-        }else{
+        } else {
             response.status(200).send('<h1>You entered Wrong Credentials</h1>')
         }
-    }catch(e) {
+    } catch (e) {
         response.status(500).send("Error" + e);
     }
+})
+
+app.get('/chair', (request, response) => {
+    // console.log(path.join(__dirname));
+    response.sendFile(path.join(__dirname, 'public_html', 'chairResponse.html'));
 })
 
 app.listen(8000, () => {
