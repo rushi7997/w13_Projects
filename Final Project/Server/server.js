759aaaa5ce12fc14c3e7fc5d5cdf4a15ad30ef47
Rushi Rami <ramirushi7997@gmail.com>
2020-12-02 16:12:22 -0500

    final project changes

--- Final Project/Server/server.js
+++ Final Project/Server/server.js
@@ -2,13 +2,13 @@
 const express = require('express');
 const app = express();
 
-const cors = require('cors')
-const logger = require('morgan')
+const cors = require('cors');
+const logger = require('morgan');
 
-app.use(express.static('public_html'))
-app.use(cors())
+app.use(express.static('public_html'));
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 app.use(logger('dev'));
 
 app.use(express.urlencoded({
